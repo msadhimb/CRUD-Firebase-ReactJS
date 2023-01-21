1afea8bd0f0c0ef8b35c57a2f717dd4e34073df8
msadhimb <adhimbaqy72@gmail.com>
2023-01-21 20:35:39 +0700

    3 commit

--- src/config/config.js
+++ src/config/config.js
@@ -10,7 +10,7 @@ import { getFirestore } from "@firebase/firestore";
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: "crud-firebase-e30f6",
+  projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_APP_ID,
